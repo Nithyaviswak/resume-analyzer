0af214cde9bb488eafb8ba8dd4076b5fa7c37249
Nithyaviswak <nviswaks@gmail.com>
2025-11-26 14:27:05 +0530

    Update app with Firebase auth and fixes

--- vite.config.js
+++ vite.config.js
@@ -5,17 +5,16 @@ import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Explicitly tell Vite that "root" is the current folder
   root: process.cwd(),
   server: {
-    port: 4000,
-    strictPort: true,
+    port: 5173, // Explicitly set to 5173
+    strictPort: true, // Fail if 5173 is busy (instead of switching)
+    host: true, // Allow network access
     fs: {
       strict: false,
     },
   },
   build: {
-  
     rollupOptions: {
       input: {
         main: resolve(process.cwd(), 'index.html'),
